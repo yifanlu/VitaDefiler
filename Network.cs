cf58aee0b232512956b062b9c3885ccef66bd934
Yifan Lu <me@yifanlu.com>
2013-11-24 13:42:29 -0800

    Finished network stuff

--- Network.cs
+++ Network.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Net;
+using System.Net.Sockets;
 
 namespace VitaDefiler
 {
@@ -21,6 +23,39 @@ namespace VitaDefiler
 
     class Network
     {
+        private Socket _sock;
+
+        public bool Connect(string host, int port)
+        {
+            Disconnect(); // disconnect previous connection
+            try
+            {
+                IPHostEntry ipHostInfo = Dns.GetHostEntry(host);
+                IPAddress ipAddress = ipHostInfo.AddressList[0];
+                IPEndPoint remoteEP = new IPEndPoint(ipAddress,port);
+
+                _sock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                _sock.Connect(remoteEP);
+                _sock.SendTimeout = 10000;
+                _sock.ReceiveTimeout = 10000;
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine("Error connecting to Vita network: {0}", ex.Message);
+                return false;
+            }
+        }
+
+        public void Disconnect()
+        {
+            if (_sock != null)
+            {
+                _sock.Shutdown(SocketShutdown.Both);
+                _sock.Close();
+            }
+        }
+
         public int RunCommand(Command cmd)
         {
             byte[] ret;
@@ -35,18 +70,64 @@ namespace VitaDefiler
 
         public int RunCommand(Command cmd, byte[] data)
         {
-            return 0;
-        }
-
-        public int RunCommand(Command cmd, byte[] data, out byte[] response)
-        {
-            response = null;
-            return 0;
+            byte[] resp;
+            if (RunCommand(cmd, data, out resp) != Command.Error)
+            {
+                return BitConverter.ToInt32(resp, 0);
+            }
+            else
+            {
+                return -1;
+            }
         }
 
         public void RunCommand(Command cmd, out byte[] data)
         {
-            data = null;
+            RunCommand(cmd, new byte[0], out data);
+        }
+
+        public Command RunCommand(Command cmd, byte[] data, out byte[] response)
+        {
+            try
+            {
+                // create and send packet
+                byte[] packet = new byte[data.Length + 8];
+                BitConverter.GetBytes(((ulong)cmd << 32) | (uint)data.Length);
+                Array.Copy(BitConverter.GetBytes((int)cmd), 0, packet, 0, 4);
+                Array.Copy(BitConverter.GetBytes(data.Length), 0, packet, 4, 4);
+                Array.Copy(data, 0, packet, 8, data.Length);
+                _sock.Send(packet);
+                // recieve packet
+                int length = 8;
+                int total = 0;
+                byte[] recv = new byte[8];
+                while (total < length)
+                {
+                    total += _sock.Receive(recv, total, length - total, SocketFlags.None);
+                }
+                Command resp = (Command)BitConverter.ToInt32(recv, 0);
+                length = BitConverter.ToInt32(recv, 4);
+                total = 0;
+                // recieve response
+                response = new byte[length];
+                while (total < length)
+                {
+                    total += _sock.Receive(response, total, length - total, SocketFlags.None);
+                }
+                // check for error
+                if (resp == Command.Error)
+                {
+                    Console.Error.WriteLine("Error from Vita: {0}", Encoding.ASCII.GetString(response));
+                    response = new byte[0];
+                }
+                return cmd;
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine("Error trying to run command {0}: {1}", cmd, ex.Message);
+                response = new byte[0];
+                return Command.Error;
+            }
         }
     }
 }
