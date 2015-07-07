920f9477472d24348ea56dbb5428520800ee38e4
Glenn Anderson <gandersonmail@yahoo.com>
2015-07-07 02:59:11 -0400

    Fixes some sort of timing issue that was causing the "VM not suspended" error to happen a lot.

--- VitaDefilerClientUnity/Assets/VitaDefilerClientUnity.cs
+++ VitaDefilerClientUnity/Assets/VitaDefilerClientUnity.cs
@@ -18,14 +18,17 @@ public class VitaDefilerClientUnity : MonoBehaviour {
 	void Start () {
 		logtext = GameObject.Find ("log_text").guiText;
 		VitaDefilerClient.AppMain.Start ();
+
+		StartCoroutine(StartListenerDelayed());
 	}
 
-	void OnEnable () {
-		Application.RegisterLogCallbackThreaded (Log);
-	}
+	private IEnumerator StartListenerDelayed()
+	{
+		yield return new WaitForSeconds(4.0f); // This needs to happen to prevent the "VM not suspended" error from happening.
 
-	void OnDisable () {
-		Application.RegisterLogCallbackThreaded (null);
+		// This can be called right away because AcceptSocket will wait until VitaDefiler
+		// connects to it, which is after privileges have been escalated.
+		VitaDefilerClient.CommandListener.StartListener();
 	}
 
 	public void Log(string logString, string stackTrace, LogType type)
@@ -35,6 +38,8 @@ public class VitaDefilerClientUnity : MonoBehaviour {
 	
 	public static void LogLine (string line)
 	{
+		Debug.Log(line);
+
 		int lines = log.Length - log.Replace("\n", "").Length;
 		if (lines >= LOG_SIZE)
 		{
